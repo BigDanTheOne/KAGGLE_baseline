0a2c76bd9fcc02fef8cf66a5ffe6cecdfb077f2b
akopianDA <230620ada>
2019-10-21 01:20:47 +0300

    setup upgrade

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
+import subprocess
+subprocess.call(["pip", "install", "-r", "requirements.txt"])
 
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='BigDan',
-    license='',
 )
+
